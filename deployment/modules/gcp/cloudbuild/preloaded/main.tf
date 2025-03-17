5fd3782da20d98acbcfa504607b8b2ec6fbc0699
Philippe Boneff <phb@google.com>
2025-03-17 14:47:04 +0000

    trigger via URL

--- deployment/modules/gcp/cloudbuild/preloaded/main.tf
+++ deployment/modules/gcp/cloudbuild/preloaded/main.tf
@@ -22,6 +22,7 @@ module "artifactregistry" {
 
 locals {
   cloudbuild_service_account   = "cloudbuild-${var.env}-sa@${var.project_id}.iam.gserviceaccount.com"
+  scheduler_service_account    = "scheduler-${var.env}-sa@${var.project_id}.iam.gserviceaccount.com"
   artifact_repo                = "${var.location}-docker.pkg.dev/${var.project_id}/${module.artifactregistry.docker.name}"
   conformance_gcp_docker_image = "${local.artifact_repo}/conformance-gcp"
 }
@@ -139,6 +140,8 @@ resource "google_cloudbuild_trigger" "build_trigger" {
 
     ## TODO(phboneff): move to its own container.
     ## Test against the conformance server with CT Preloader.
+    ## Leave enough time for the preloader to run, until the token expires.
+    timeout = "3600s" // 60 minutes
     step {
       id       = "ct_preloader"
       name     = "golang"
@@ -148,10 +151,14 @@ resource "google_cloudbuild_trigger" "build_trigger" {
         go run github.com/google/certificate-transparency-go/preload/preloader@master \
           --target_log_uri=$(cat /workspace/conformance_url)/arche2025h1.ct.transparency.dev \
 	        --target_bearer_token="$(cat /workspace/cb_identity)" \
-          --source_log_uri=https://ct.googleapis.com/logs/us1/argon2025h1
-	        --start_index=$START_INDEX
+          --source_log_uri=https://ct.googleapis.com/logs/us1/argon2025h1 \
+	        --start_index=$START_INDEX \
+          --num_workers=10 \
+          --parallel_fetch=10 \
+          --parallel_submit=10
       EOT
       wait_for = ["bearer_token"]
+      timeout = "3000s" // 50 minutes
     }
 
     options {
@@ -163,4 +170,34 @@ resource "google_cloudbuild_trigger" "build_trigger" {
   depends_on = [
     module.artifactregistry
   ]
+}
+
+resource "google_cloud_scheduler_job" "deploy_cron" {
+  paused = false
+  project = var.project_id
+  region  = var.location
+  name    = "deploy-cron"
+
+  schedule  = "*/50 * * * *"
+  time_zone = "America/Los_Angeles"
+
+  attempt_deadline = "120s"
+
+  // TODO(phboneff): use a batch job instead maybe
+  http_target {
+    http_method = "POST"
+    uri         = "https://cloudbuild.googleapis.com/v1/projects/${var.project_id}/locations/${var.location}/triggers/${google_cloudbuild_trigger.build_trigger.trigger_id}:run"
+    body        = base64encode(jsonencode({
+      source = {
+        branchName = "preloader"
+      }
+    }))
+    headers = {
+      "Content-Type" = "application/json"
+    }
+
+    oauth_token {
+      service_account_email = local.scheduler_service_account
+    }
+  }
 }
